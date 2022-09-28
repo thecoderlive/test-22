a1087b91ea14e17eabc2849dce8f022af1e44a56
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 17:50:20 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "46c3cc8b-0eee-4b80-b36d-3b7cfccd09cb",
+    "home_feed": [
+        {
+            "id": "f429e7f5-6a6b-41b5-b3b5-264124afd160",
+            "artist_name": "second",
+            "home_carousel": [
+                {
+                    "id": "1ef169f3-6552-4f92-9d93-45af179f5a95",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "b1c3727d-ffe5-417d-a6d2-05479ccf3bac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "5e274a21-1305-46c2-ac5c-f07599e48f66",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "ac9999b9-541e-4f6f-b4f2-dbfa2035d7b2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/292.png"
+                },
+                {
+                    "id": "ff2d0ee2-93f1-4e71-a567-cdcf38d1d1d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                }
+            ],
+            "art_caption": "child",
+            "art_description": "No whose thousand accept suffer."
+        },
+        {
+            "id": "58953d26-e7c7-431b-8fca-921356afa187",
+            "artist_name": "walk",
+            "home_carousel": [
+                {
+                    "id": "4368f22d-87e0-41ee-a993-94e996fa5832",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "f5fc39d0-8b8c-4fa2-ad20-3c4390eea22f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                },
+                {
+                    "id": "e79f81c5-a0a2-4076-89ad-aaa01d5d31b3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+                },
+                {
+                    "id": "ff1d911f-5685-4827-9c58-54bbc7916d5d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "b7a59cc6-cb6f-4b2e-9779-d6b01745fc41",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+                }
+            ],
+            "art_caption": "movie",
+            "art_description": "Stay her son school member sort research. Open type leader green. Pay six have sound wind."
+        },
+        {
+            "id": "98dcaa0b-04b4-4896-ac63-0586130c1c99",
+            "artist_name": "good",
+            "home_carousel": [
+                {
+                    "id": "b5e70470-f8f4-4be3-8c07-217990b103a5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "e23cce99-31f9-4ee6-9e4e-036fe3a65eed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/343.png"
+                },
+                {
+                    "id": "8b0f3010-94c4-4cef-a8ae-a75d0fba2452",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                },
+                {
+                    "id": "73fd02a6-42a3-4cae-aec0-2cd06abcc42b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "96cad186-ddaa-4ade-82a9-b3c95d4f68ee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+                }
+            ],
+            "art_caption": "lay",
+            "art_description": "Policy her after town. Impact friend window approach common fill subject. Analysis help Congress election turn produce able."
+        },
+        {
+            "id": "75ceafac-0003-4fe6-b7ba-18903ac3066b",
+            "artist_name": "black",
+            "home_carousel": [
+                {
+                    "id": "ed7d4eb0-fe73-4357-b052-a1c975714dfc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/320.png"
+                },
+                {
+                    "id": "01dcc70e-637b-4d8b-a6b9-12ba7cecb994",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "ebcb0fd3-102b-4929-b338-57ff44232e83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                },
+                {
+                    "id": "1202afbc-70ed-4e8e-8c1c-f79b43553574",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                },
+                {
+                    "id": "a2c12a46-bd9d-463e-900e-8a5f8221f856",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                }
+            ],
+            "art_caption": "sense",
+            "art_description": "Year upon involve recent indeed reality. Play law knowledge role heavy red weight. Fine data our sell. Turn walk when time decision whom."
+        },
+        {
+            "id": "83f666e2-fbd1-464d-a72d-5e9d1d2023f7",
+            "artist_name": "Congress",
+            "home_carousel": [
+                {
+                    "id": "1f939f51-1a06-460e-8e96-cd19cbbe3f67",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "c5b31932-e517-492e-a8da-f381f8bae17a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/324.png"
+                },
+                {
+                    "id": "7ba81f5b-bf13-4491-b166-b434ae462ac2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png"
+                },
+                {
+                    "id": "2bec573f-d286-4742-acc7-cfe5c44195b1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png"
+                },
+                {
+                    "id": "7cacb6d8-05a3-4388-8fea-75e7edbb0eb3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/316.png"
+                }
+            ],
+            "art_caption": "tough",
+            "art_description": "Sea station buy. Foot father space employee must coach morning."
+        }
+    ]
+}
